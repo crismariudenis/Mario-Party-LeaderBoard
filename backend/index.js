b3243a4b115d0818caea166d23f00761efab8b80
crismariudenis <deniscrismariu@gmail.com>
2024-12-19 15:06:40 +0200

    changed the backend to work with every ip

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 
-
-
-
 const app = express();
 app.use(bodyParser.json());
 app.use(
@@ -15,7 +12,6 @@ app.use(
   })
 );
 
-
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
@@ -23,7 +19,6 @@ mongoose
   .catch((err) => console.error("MongoDB connection error:", err));
 
 // Define Mongoose Schemas
-
 
 const submissionSchema = new mongoose.Schema({
   games: [{ place: Number, players: [String], bonus: Number }],
@@ -36,8 +31,8 @@ const Submission = mongoose.model("Submission", submissionSchema);
 // Fetch all games
 app.get("/submissions", async (req, res) => {
   try {
-      const games = await Submission.find();
-      console.log(games);
+    const games = await Submission.find();
+    console.log(games);
     res.json(games);
   } catch (error) {
     res.status(500).json({ message: "Failed to fetch games" });
@@ -65,6 +60,6 @@ app.post("/submissions", async (req, res) => {
 
 // Start the server
 const PORT = 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });

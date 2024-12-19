c13103771ce760160798b7cd8b7fb0df34e02293
crismariudenis <deniscrismariu@gmail.com>
2024-12-19 15:27:52 +0200

    vercel.json

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 
+
+
+
 const app = express();
 app.use(bodyParser.json());
 app.use(
@@ -12,6 +15,7 @@ app.use(
   })
 );
 
+
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
@@ -19,6 +23,7 @@ mongoose
   .catch((err) => console.error("MongoDB connection error:", err));
 
 // Define Mongoose Schemas
+
 
 const submissionSchema = new mongoose.Schema({
   games: [{ place: Number, players: [String], bonus: Number }],
@@ -31,8 +36,8 @@ const Submission = mongoose.model("Submission", submissionSchema);
 // Fetch all games
 app.get("/submissions", async (req, res) => {
   try {
-    const games = await Submission.find();
-    console.log(games);
+      const games = await Submission.find();
+      console.log(games);
     res.json(games);
   } catch (error) {
     res.status(500).json({ message: "Failed to fetch games" });
@@ -60,6 +65,6 @@ app.post("/submissions", async (req, res) => {
 
 // Start the server
 const PORT = 5000;
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
